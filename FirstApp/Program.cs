bf3dd065d17f3262f147f09acecb62c1c3b2908c
Goncha <gogono76@gmail.com>
2019-02-25 17:34:34 +0200

    `

---
+++ FirstApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, bio;
+            int age, n;
+            List<User> user = new List<User> ();
+            n = Convert.ToInt32 (Console.ReadLine ());
+            for (int i = 0; i < n; i++)
+            {
+                name = Console.ReadLine();
+                bio = Console.ReadLine();
+                age = Convert.ToInt32(Console.ReadLine());
+                user.Add(new User (name, age, bio));
+            }
+            foreach (var e in user)
+            {
+                e.GetInfo();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ FirstApp/user.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+    class User
+    {
+        public string Name;
+        public int Age;
+        public string Bio;
+        public User()
+        {
+            
+        }
+        public User(string name, int age, string bio)
+        {
+            Name = name;
+            Age = age;
+            Bio = bio;
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine($"Imya {Name}\nAge {Age}\nBio {Bio}\n");
+        }
+    }
+}
